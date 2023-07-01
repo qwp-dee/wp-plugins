86c25d0a8230ee309b8c373bf6df5d8b4da668ba
WP-Developer <55971399+wp-deepak@users.noreply.github.com>
2023-07-01 14:06:37 +0530

    Update plurals.php
    changes

--- wp-post-formats/includes/plurals.php
+++ wp-post-formats/includes/plurals.php
@@ -4,17 +4,9 @@
 add_filter( 'single_term_title', 'my_post_format_single_term_title' );
 
 /**
- * Filters the single post format title, which is used on the term archive page. The purpose of this
+ * Filters the single post format title used on the term archive page. The purpose of this
  * function is to replace the singular name with a plural version.
  *
- * @author Justin Tadlock
- * @copyright Copyright (c) 2012
- * @link http://justintadlock.com/archives/2012/09/07/post-format-plural-strings
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * @since 0.1.0
- * @access public
- * @param string $title The term name.
- * @return string
  */
 function my_post_format_single_term_title( $title ) {
 
@@ -23,21 +15,11 @@ function my_post_format_single_term_title( $title ) {
 		$plural = my_post_format_get_plural_string( $term->slug );
 		$title = !empty( $plural ) ? $plural : $title;
 	}
-
 	return $title;
 }
 
 /**
  * Gets the plural version of a post format name.
- *
- * @author Justin Tadlock
- * @copyright Copyright (c) 2012
- * @link http://justintadlock.com/archives/2012/09/07/post-format-plural-strings
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * @since 0.1.0
- * @access public
- * @param string $slug The term slug.
- * @return string
  */
 function my_post_format_get_plural_string( $slug ) {
 
@@ -48,19 +30,6 @@ function my_post_format_get_plural_string( $slug ) {
 	return isset( $strings[ $slug ] ) ? $strings[ $slug ] : '';
 }
 
-/**
- * Defines plural versions of the post format names since WordPress only provides a singular version
- * of each format. Basically, I hate having archive pages labeled with the singular name, so this is
- * what I created to take care of that problem.
- *
- * @author Justin Tadlock
- * @copyright Copyright (c) 2012
- * @link http://justintadlock.com/archives/2012/09/07/post-format-plural-strings
- * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * @since 0.1.0
- * @access public
- * @return array
- */
 function my_post_format_get_plural_strings() {
 
 	$strings = array(
